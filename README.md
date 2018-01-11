69858b28614915bceaf2a3ef702333deee34e649
greek-geek <shraddha.m26@gmail.com>
2018-01-10 19:41:10 -0800

    update 1

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Brat standoff to CDLI-CoNLL Converter
 
 ## Installation
 
+To install this converter, run the following commands:
+
 ```
 pip install git+git://github.com/cdli-gh/brat_to_cdli_conll_converter.git
 ```
--- brat2conll/cli.py
+++ brat2conll/cli.py
@@ -42,6 +42,7 @@ def check_and_process(pathname, verbose=False):
 @click.option('-v', '--verbose', default=False, required=False, is_flag=True, help='Enables verbose mode')
 @click.version_option()
 def main(input_path, verbose):
+    print(input_path)
     if os.path.isdir(input_path):
         with click.progressbar(os.listdir(input_path), label='Info: Converting the files') as bar:
             for f in bar:
